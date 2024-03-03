9e709d14c104d9de951d707d23b9ae6b5a59e13f
Joy <github.joysaha@gmail.com>
2024-03-03 16:09:55 +0600

    successfully fetch data from api link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("check")
+
+
+//All post function and fetch api
+
+const loadData = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    const newsData = data.posts;
+    console.log(newsData)
+}
+
+
+
+
+loadData()
